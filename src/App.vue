fae22ef2cfe3304a0bb167013cbe1020f6c1375c
Aniket Aryamane <aryamane.aniket@gmail.com>
2026-06-10 20:21:54 +0530

    Adding tailwind classes

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,11 @@ const msg = ref<taskFilters>('all')
 </script>
 
 <template>
-	<h1>{{ msg }}</h1>
-	<Child
-		:taskFilters="TASK_FILTERS"
-		v-model:f="msg"
-	/>
+	<div class="container mx-auto my-2">
+		<h1 class="text-2xl">{{ msg ? msg.charAt(0).toUpperCase() + msg.slice(1) : msg }}</h1>
+		<Child
+			:taskFilters="TASK_FILTERS"
+			v-model:f="msg"
+		/>
+	</div>
 </template>
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -13,13 +13,15 @@ const updateFilter = (filter: TaskFilter): void => {
 </script>
 
 <template>
-	<div>
+	<div class="flex gap-4">
 		<button
 			@click="updateFilter(filter)"
 			v-for="(filter, index) in taskFilters"
 			:key="index"
+			class="my-4 rounded px-4 py-2 font-bold text-white hover:bg-blue-700"
+			:class="currentFilter === filter ? 'bg-amber-600' : 'bg-amber-300'"
 		>
-			{{ filter }}
+			{{ filter ? filter.charAt(0).toUpperCase() + filter.slice(1) : filter }}
 		</button>
 	</div>
 </template>
