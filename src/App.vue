13a8f6029e8d9d18d4135b24c207819c461a7767
Aniket Aryamane <aryamane.aniket@gmail.com>
2026-06-10 23:16:44 +0530

    kebab case for props in parent

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ const msg = ref<TaskFilter>('all')
 	<div class="container mx-auto my-2">
 		<h1 class="text-2xl">{{ msg ? msg.charAt(0).toUpperCase() + msg.slice(1) : msg }}</h1>
 		<Child
-			:taskFilters="TASK_FILTERS"
+			:task-filters="TASK_FILTERS"
 			v-model:f="msg"
 		/>
 	</div>
